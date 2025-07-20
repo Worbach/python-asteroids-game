df7e493c198735e2564acc2580a7d2d97e9800db
Worbach <will_orbach@yahoo.com>
2025-07-19 20:51:29 -0400

    Created a splitting mechanic with you shoot large and medium asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,8 @@
 import pygame
+import random
+
 from circleshape import CircleShape
+from constants import *
 
 
 class Asteroid(CircleShape):
@@ -11,3 +14,24 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # randomize the angle of the split
+        random_angle = random.uniform(20, 50)
+
+        debris_velocity_pos_angle = self.velocity.rotate(random_angle)
+        debris_velocity_neg_angle = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        # Asteroid debris broken into 2 pieces in different directions
+        asteroid_debris1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_debris1.velocity = debris_velocity_pos_angle * 1.2
+
+        asteroid_debris2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_debris2.velocity = debris_velocity_neg_angle * 1.2
